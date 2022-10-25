7af83cf7bfec8594a4e492326cbebd80e426f256
Robert Jensen <robert@robert-jensen.dk>
2022-10-25 05:56:38 +0200

    added test path

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World from FastAPI Accelerator"}
+
+@app.get("/dell")
+async def dell():
+    return {"message": "Hello World from Dell conf"}
