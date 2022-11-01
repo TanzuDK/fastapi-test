8202cf500a8f0ea44cb424d2162916a0fa2bd9f6
Robert Jensen <robert@robert-jensen.dk>
2022-11-01 21:55:35 +0100

    added cors

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://tap-gui.tap.tanzu.dk",
+    "https://tap-gui.tap.tanzu.dk",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World from FastAPI Accelerator with Auto API registration!!!"}
+    return {
+        "message": "Hello World from FastAPI Accelerator with Auto API registration!!!"
+    }
+
 
 @app.get("/api1")
 async def devopsconf1():
     return {"message": "Hello from api 1"}
+
 
 @app.get("/api2")
 async def devopsconf2():
